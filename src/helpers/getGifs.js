aa3f9d9d0cf30b4ab79d381e35f90c4eb2e7cfb6
Alexis Salgado <chimalexoficial@gmail.com>
2024-05-31 13:15:12 -0600

    build

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${category}&limit=12`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=3IZBjTVav4NJN1WDLFej5WvoHO3T5Qjf&q=${category}&limit=12`;
 
     const res = await fetch(url);
     const { data } = await res.json();
