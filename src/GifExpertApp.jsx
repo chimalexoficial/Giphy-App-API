9a17c4a1145607df224c19d5797a45e1280690f8
chimalexoficial <chimalexoficial@gmail.com>
2025-10-21 18:34:25 -0600

    Test CI/CD deployment

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -14,6 +14,10 @@ export default function GifExpertApp() {
     return (
         <div className="container mx-auto">
             <h1 className=" mt-10 text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">Giphy API App</h1>
+
+            {/* test for ci/cd */}
+            <h2 className=" mt-10 text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">And deployed via GitHub Actions</h2>
+
             <p className="text-center text-2xl">
                 Giphy App is a dynamic React application that leverages the power of the Giphy API to provide users with a fun and interactive way to explore GIFs. Built with React.js, Tailwind CSS, and component-based architecture, Giphy App offers a seamless and visually appealing experience for discovering and sharing GIFs across the web.</p>
             <AddCategory
